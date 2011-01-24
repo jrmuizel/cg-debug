31bb70068300822348b371afe08df25ba8a099d0
Jeff Muizelaar <jmuizelaar@mozilla.com>
2011-01-24 16:13:41 -0500

    Add support for verbose logging and dumping the options

--- cg-debug.c
+++ cg-debug.c
@@ -1,8 +1,32 @@
 #include "CGSInternal.h"
 
+void dump_options()
+{
+	int options;
+	CGSGetDebugOptions(&options);
+	printf("%x\n", options);
+#define OPTION_NAME(a) if (options & a) printf(#a "\n");
+	OPTION_NAME(kCGSDebugOptionNone);
+	OPTION_NAME(kCGSDebugOptionFlashScreenUpdates);
+	OPTION_NAME(kCGSDebugOptionColorByAccelleration);
+	OPTION_NAME(kCGSDebugOptionNoShadows);
+	OPTION_NAME(kCGSDebugOptionNoDelayAfterFlash);
+	OPTION_NAME(kCGSDebugOptionAutoflushDrawing);
+	OPTION_NAME(kCGSDebugOptionShowMouseTrackingAreas);
+	OPTION_NAME(kCGSDebugOptionFlashIdenticalUpdates);
+}
+
 int main(int argc, char **argv) {
-	if (argc < 2)
-		exit(1);
-	if (!strcmp(argv[1], "surface")) // write out to /tmp/WindowServer.sinfo.out
-		CGSSetDebugOptions(kCGSDebugOptionDumpSurfacesToFile);
+	if (argc > 1) {
+		if (!strcmp(argv[1], "surface")) {
+			printf("dumping surfaces to /tmp/WindowServer.sinfo.out\n");
+			CGSSetDebugOptions(kCGSDebugOptionDumpSurfacesToFile);
+		}
+
+		/*! Dumps a very verbose debug log of the WindowServer to /tmp/CGLog_WinServer_<PID>. */
+		if (!strcmp(argv[1], "vlog")) {
+			CGSSetDebugOptions(kCGSDebugOptionVerboseLogging);
+		}
+	}
+	dump_options();
 }
